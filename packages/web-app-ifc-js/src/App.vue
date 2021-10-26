245953320d2fa99a68bc1d8a0467b9a1de64fb38
Vasco Guita <vasco.guit@tecnico.ulisboa.pt>
2021-10-26 11:20:02 +0200

    Display three.js scene with IFC extension

--- packages/web-app-ifc-js/src/App.vue
+++ packages/web-app-ifc-js/src/App.vue
@@ -1,12 +1,10 @@
 <template>
-  <main>
-    <h1>Hello</h1>
-  </main>
+<div id="container"></div>
 </template>
 
 <script>
-
-import * as THREE from 'three';
+//import  * as THREE from 'https://cdn.skypack.dev/pin/three@v0.133.1-a8rkd0QTHl2tMZXZJAEw/mode=imports/optimized/three.js'
+import * as THREE from "three"
 
 export default {
   name: 'IFCEditor',
@@ -19,21 +17,34 @@ export default {
   },
   methods: {
     init: function() {
+      let container = document.getElementById('container');
       this.scene = new THREE.Scene()
       this.camera = new THREE.PerspectiveCamera(75,
-        window.innerWidth/window.innerHeight, 0.1, 1000)
-      this.renderer = new THREE.WebGLRenderer()
-      renderer.setSize(window.innerWidth, window.innerHeight)
-      document.body.appendChild(renderer.domElement)
+        container.clientWidth / container.clientHeight, 0.1, 1000)
+      this.renderer = new THREE.WebGLRenderer({antialias: true})
+      this.renderer.setSize(container.clientWidth, container.clientHeight)
+      container.appendChild(this.renderer.domElement);
+    },
+    addCube: function() {
+      let geometry = new THREE.BoxGeometry(1, 1, 1);
+      let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      let cube = new THREE.Mesh(geometry, material);
+      this.scene.add(cube);
+
+      this.camera.position.z = 5;
     },
     animate: function() {
-      requestAnimationFrame(this.animate)
       this.renderer.render(this.scene, this.camera)
     }
   },
   mounted() {
     this.init()
+    this.addCube()
     this.animate()
   }
 }
 </script>
+
+<style>
+  #container {width: 100%; height: 100%}
+</style>

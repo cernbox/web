6685468dbe7a5fc007b31b251f980010f871a023
Diogo Castro <diogo.castro@cern.ch>
2022-10-19 11:28:52 +0200

    PENDING PR: ensure proper sorting for shared with (when sharing with user and/or multiple groups)

--- packages/web-app-files/src/helpers/ui/resourceTable.ts
+++ packages/web-app-files/src/helpers/ui/resourceTable.ts
@@ -25,7 +25,17 @@ export const determineSortFields = (firstResource): SortField[] => {
       name: 'sharedWith',
       sortable: (sharedWith) => {
         if (sharedWith.length > 0) {
-          return sharedWith[0].displayName
+          // Ensure the sharees are always sorted and that user
+          // take precedence over groups, and then return a
+          // string with all elements.
+          // This ensures shares with multiple shares do not
+          // appear mixed within others with a single one
+          return sharedWith.sort((a, b) => {
+            if (a.shareType !== b.shareType) {
+              return a.shareType < b.shareType ? -1 : 1
+            }
+            return a.displayName < b.displayName ? -1 : 1
+          }).map((e) => e.displayName).join()
         }
         return false
       },

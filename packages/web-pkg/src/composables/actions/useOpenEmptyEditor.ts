165eb167be1afab83758670c7fc07b81769059fd
Rodrigo <rodrigo.pavao.coffani.nunes@cern.ch>
2025-12-04 15:12:13 +0100

    fix: add file to resources after creating it

--- packages/web-pkg/src/composables/actions/useOpenEmptyEditor.ts
+++ packages/web-pkg/src/composables/actions/useOpenEmptyEditor.ts
@@ -44,6 +44,8 @@ export const useOpenEmptyEditor = () => {
       path: urlJoin(filePath, fileName)
     })
 
+    resourcesStore.upsertResource(emptyResource)
+
     const space = getMatchingSpace(emptyResource)
     const appFileExtension = appsStore.fileExtensions.find(
       ({ app, extension: ext }) => app === appId && ext === extension

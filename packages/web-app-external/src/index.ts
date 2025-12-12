58b87c5f3387ea25e4994be95b9db6e68e4c22e6
Diogo Castro <diogo.castro@cern.ch>
2025-12-12 10:40:28 +0100

    Workaround: friendly name for collabora and office

--- packages/web-app-external/src/index.ts
+++ packages/web-app-external/src/index.ts
@@ -56,9 +56,15 @@ export default defineWebApplication({
       id: appId,
       extensions: mimeTypes.map((mimeType) => {
         const provider = mimeType.app_providers.find((provider) => provider.name === appName)
+        let visible_name = provider.name
+        if (provider.name === 'MS365') {
+          visible_name = 'Microsoft Office 365'
+        } else if (provider.name === 'Collabora') {
+          visible_name = 'Collabora (experimental)'
+        }
         return {
           extension: mimeType.ext,
-          label: () => $gettext('Open in %{app}', { app: provider.name }),
+          label: () => $gettext('Open in %{app}', { app: visible_name }),
           icon: provider.icon,
           name: provider.name,
           mimeType: mimeType.mime_type,

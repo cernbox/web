3030a1155a041d1d63b1b22bdb2bfed690209fe5
arthurarabelo <arthurrabelo2015@hotmail.com>
2026-09-17 17:14:13 +0200

    correct portal name

--- packages/web-runtime/src/App.vue
+++ packages/web-runtime/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <portal-target name="app.app-banner" multiple />
-  <portal-target name="app.runtime.plainPages" multiple />
+  <portal-target name="app.runtime.allPages" multiple />
   <div id="web">
     <oc-hidden-announcer :announcement="announcement" level="polite" />
     <skip-to target="web-content-main">

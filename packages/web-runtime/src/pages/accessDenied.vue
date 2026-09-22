98267923a02c3464fe9f0f7e1d117fe83c9b360c
Diogo Castro <diogo.castro@cern.ch>
2026-09-22 15:47:24 +0200

    Revert "Display otg message on access denied page"
    This reverts commit 6e7c30b321dc88e51f01335306fbd23274372f24.

--- packages/web-runtime/src/pages/accessDenied.vue
+++ packages/web-runtime/src/pages/accessDenied.vue
@@ -33,10 +33,6 @@
     >
       {{ navigateToLoginText }}
     </oc-button>
-    <div class="snackbars">
-      <portal-target name="app.runtime.accessDenied" />
-      <message-bar />
-    </div>
   </div>
 </template>
 
@@ -52,13 +48,9 @@ import {
   useThemeStore,
   useAuthService
 } from '@ownclouders/web-pkg'
-import MessageBar from '../components/MessageBar.vue'
 
 export default defineComponent({
   name: 'AccessDeniedPage',
-  components: {
-    MessageBar
-  },
   setup() {
     const themeStore = useThemeStore()
     const { currentTheme } = storeToRefs(themeStore)
@@ -150,20 +142,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style lang="scss" scoped>
-.snackbars {
-  position: absolute;
-  right: 20px;
-  bottom: 20px;
-  z-index: calc(var(--oc-z-index-modal) + 1);
-
-  @media (max-width: 640px) {
-    left: 0;
-    right: 0;
-    margin: 0 auto;
-    width: 100%;
-    max-width: 500px;
-  }
-}
-</style>

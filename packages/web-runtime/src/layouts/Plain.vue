b8b9c9e4288ec0be9eb930328668c1d5c3525026
Diogo Castro <diogo.castro@cern.ch>
2026-09-22 15:47:24 +0200

    Revert "Expose target to otg portal for the whole application"
    This reverts commit 0a85de269d16fa9ee1c06c59ff70c60dabded3a3.

--- packages/web-runtime/src/layouts/Plain.vue
+++ packages/web-runtime/src/layouts/Plain.vue
@@ -5,9 +5,6 @@
   >
     <h1 class="oc-invisible-sr" v-text="pageTitle" />
     <router-view />
-    <div class="snackbars">
-      <message-bar />
-    </div>
   </div>
 </template>
 
@@ -16,13 +13,9 @@ import { storeToRefs } from 'pinia'
 import { computed, defineComponent, unref } from 'vue'
 import { useGettext } from 'vue3-gettext'
 import { useRouteMeta, useThemeStore } from '@ownclouders/web-pkg'
-import MessageBar from '../components/MessageBar.vue'
 
 export default defineComponent({
   name: 'PlainLayout',
-  components: {
-    MessageBar
-  },
   setup() {
     const { $gettext } = useGettext()
     const themeStore = useThemeStore()
@@ -42,20 +35,3 @@ export default defineComponent({
   }
 })
 </script>
-
-<style lang="scss" scoped>
-.snackbars {
-  position: absolute;
-  right: 20px;
-  bottom: 20px;
-  z-index: calc(var(--oc-z-index-modal) + 1);
-
-  @media (max-width: 640px) {
-    left: 0;
-    right: 0;
-    margin: 0 auto;
-    width: 100%;
-    max-width: 500px;
-  }
-}
-</style>
